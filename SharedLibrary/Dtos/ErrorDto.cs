f16c0e86238a5c6a9165db5c64bb69f63099a861
Elif ÇALIŞ <pcengelf@hotmail.com>
2024-09-19 18:55:08 +0200

    Add project files.

---
+++ SharedLibrary/Dtos/ErrorDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Dtos
+{
+
+    //Apilerde herhangi bir hata oldugunda merkezi error oluşturuyoruz.
+    public class ErrorDto
+    {
+        //Birden fazla hata olabilir.O yüzden liste olarak oluşturuyoruz.
+        public  List<String> Errors { get;private set; } //Private vermemizin sebebi bu propertyi burada set edeceğiz.Dışarıdan set edilmeyecek.
+        public  bool IsShow { get;private set; } //Gelen hatayı göstermek için oluşturulan property.Kullanıcıya gösterilecek hatalarda true olacak.Yazılımcıya gösterilecek hata da false olacak.
+
+        public ErrorDto()
+        {
+            Errors = new List<string>();
+
+        }
+        public ErrorDto(string error , bool isShow)
+        {
+            Errors.Add(error);
+            isShow = true;
+        }
+        public ErrorDto(List<string> errors,bool isShow)
+        {
+            Errors = errors;
+            IsShow = isShow;
+           
+        }
+    }
+}
---
+++ SharedLibrary/Dtos/Response.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Dtos
+{
+    public class Response<T> where T : class
+    {
+        public T Data { get; private set; }
+        public int StatusCode { get; private set; } //Client başka bir yerde kullanacağı zaman lazım olabilir.Private vermemizin sebebi bu propertyi burada set edeceğiz.Dışarıdan set edilmeyecek.
+
+        [JsonIgnore] //Jsondataya dönüştürüldüğünde bu propertyi yok edilecek.
+        public  bool IsSuccessful { get;private set; } //Kendi apilerimde başarılı olup olmadığına bakmak için yapıyorum.
+        public ErrorDto Error { get;private set; }
+
+        public static Response<T> Success(T data, int statusCode) //istek başarılı olduğu durumda
+        {
+            return new Response<T> { Data = data, StatusCode = statusCode ,IsSuccessful=true};
+        }
+
+        //static kullanmamızın nedeni her seferinde nesne örneği üzerinden erişmeyelim diye.
+        public static Response<T> Success(int statusCode) // istek başarılı ama data boş olduğunda,update remove işlemleri gibi
+        {
+            return new Response<T> { Data = default, StatusCode = statusCode,IsSuccessful=true};
+
+        }
+
+        public static Response<T> Fail(ErrorDto errorDto, int statusCode) //istek başarısız olduğunda
+        {
+            return new Response<T>
+            {
+                Error = errorDto,
+                StatusCode = statusCode,
+                IsSuccessful = false
+            };
+        }
+
+        // Tek bir hata döneceği zaman kullanılacak 
+        public static Response<T> Fail(string errorMessage, int statusCode, bool isShow)
+        {
+            var errorDto = new ErrorDto(errorMessage, isShow);
+            return new Response<T> { Error = errorDto, StatusCode = statusCode ,IsSuccessful=false};
+        }
+    }
+}
+
